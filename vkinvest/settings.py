5ba200f9c3d2ea6794231f5b629227ad8c64e501
Vinod Kurup <vinod@kurup.com>
2012-05-04 15:07:05 -0400

    Added django-debug-toolbar

--- vkinvest/settings.py
+++ vkinvest/settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'vkinvest.urls'
@@ -124,6 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'portfolio',
+    'debug_toolbar',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -154,3 +156,6 @@ LOGGING = {
         },
     }
 }
+
+# for django debug toolbar
+INTERNAL_IPS = ('127.0.0.1',)
