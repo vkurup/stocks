243c9efbca21cd0c49256b486d17e37c7b48eb5d
Vinod Kurup <vinod@kurup.com>
2012-05-05 20:51:45 -0400

    Some initial SQL fixtures

---
+++ portfolio/sql/transaction.sql
@@ -0,0 +1,21 @@
+insert into portfolio_transaction (action, date, security, shares, price, commission)
+  values ("DEPOSIT", "1998-05-04 00:00:00.000000", "CASH", 7500, 1.00, 0.00);
+
+insert into portfolio_transaction (action, date, security, shares, price, commission)
+  values ("BUY", "1998-05-05 00:00:00.000000", "AAPL", 10, 29.25, 14.95);
+
+insert into portfolio_transaction (action, date, security, shares, price, commission)
+  values ("BUY", "1998-05-05 00:00:00.000000", "PFE", 10, 112.75, 14.95);
+
+insert into portfolio_transaction (action, date, security, shares, price, commission)
+  values ("BUY", "1998-05-07 00:00:00.000000", "EK", 13, 73.50, 14.95);
+
+insert into portfolio_transaction (action, date, security, shares, price, commission)
+  values ("BUY", "1998-05-07 00:00:00.000000", "G", 9, 114.625, 14.95);
+
+insert into portfolio_transaction (action, date, security, shares, price, commission)
+  values ("BUY", "1998-05-07 00:00:00.000000", "GM", 14, 69.00, 14.95);
+
+insert into portfolio_transaction (action, date, security, shares, price, commission)
+  values ("BUY", "1998-05-07 00:00:00.000000", "IP", 18, 52.9375, 14.95);
+
